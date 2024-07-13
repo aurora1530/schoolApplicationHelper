8ba96b14224e53662b8bc2f60cd93bc11035ac7f
aurora1530 <35615953+aurora1530@users.noreply.github.com>
2024-07-13 16:51:08 +0900

    rename: search=>fetch

--- frontend/lib/widgets/search/search_result.dart
+++ frontend/lib/widgets/search/search_result.dart
@@ -14,7 +14,7 @@ class _SearchResultState extends State<SearchResult> {
   List<HighSchoolInfo> _schools = [];
   final TextEditingController _searchController = TextEditingController();
 
-  Future<void> _searchSchools(String query) async {
+  Future<void> _fetchSchools(String query) async {
     print('query: $query');
     // localhost => 10.0.2.2
     // cf. https://araramistudio.jimdo.com/2018/01/11/android%E3%81%AE%E3%82%A8%E3%83%9F%E3%83%A5%E3%83%AC%E3%83%BC%E3%82%BF%E3%83%BC%E3%81%8B%E3%82%89%E8%87%AA%E8%BA%AB%E3%81%AEpc-localhost-%E3%81%B8%E6%8E%A5%E7%B6%9A/
@@ -61,7 +61,7 @@ class _SearchResultState extends State<SearchResult> {
                     icon: const Icon(Icons.search),
                     onPressed: () {
                       setState(() {
-                        _searchSchools(_searchController.text);
+                        _fetchSchools(_searchController.text);
                       });
                     },
                   ),
