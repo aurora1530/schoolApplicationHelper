1d5c9f68f2178ecdfeb5c9ebcabd0e05a3029b97
aurora1530 <35615953+aurora1530@users.noreply.github.com>
2024-07-13 15:39:43 +0900

    remove: 不要なログ出力を削除

--- backend/src/lib/prisma/search.ts
+++ backend/src/lib/prisma/search.ts
@@ -3,7 +3,6 @@ import { Client } from "./prisma"
 export const searchSchoolsByName = async (nameQuery: string) => {
   const client = Client.getInstance()
 
-  console.log(nameQuery)
   const schools = await client.highSchool.findMany({
     where: {
       name: {
