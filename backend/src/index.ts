6b4c28ac2c78d6930aa20099a22cb11702607faa
aurora1530 <35615953+aurora1530@users.noreply.github.com>
2024-07-13 15:39:27 +0900

    add: クエリパラムでの高校名検索を実装

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,23 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import { searchSchoolsByName } from './lib/prisma/search'
 
 const app = new Hono()
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
+})
+
+app.get('/search', async (c) => {
+  const query = c.req.query('q') ?? ''
+  if (!query) {
+    c.status(400)
+    return c.json({  error: 'Query is required' })
+  }
+
+  const schools = await searchSchoolsByName(query)
+
+  return c.json(schools)
 })
 
 const port = 3000
