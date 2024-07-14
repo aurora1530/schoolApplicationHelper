1bd11d8634c402b2355f14a093fd256d99dd4ba1
aurora1530 <35615953+aurora1530@users.noreply.github.com>
2024-07-14 12:18:44 +0900

    feat: 出願方法のfetch関数を外部関数化

---
+++ frontend/lib/utils/fetch_applications.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:school_application_helper/widgets/search/application_info.dart';
+
+Future<List<ApplicationInfo>> fetchApplications(int schoolID) async {
+  try {
+    final response = await http
+        .get(Uri.parse('http://10.0.2.2:3000/search/highSchools/$schoolID'));
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> applicationJson = json.decode(response.body);
+      final List<dynamic> applicationListJson = applicationJson['applications'];
+      final applications = applicationListJson
+          .map((applicationJson) => ApplicationInfo.fromJson(applicationJson))
+          .toList();
+      return applications;
+    } else {
+      throw Exception('Failed to load applications');
+    }
+  } catch (e) {
+    throw Exception('Failed to load applications: $e');
+  }
+}
