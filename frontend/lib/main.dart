3984c09c2e0fee42d68b48d4f536b555a039f056
aurora1530 <35615953+aurora1530@users.noreply.github.com>
2024-07-14 14:40:27 +0900

    feat: ヘッダーのタイトルを日本語に

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
         useMaterial3: true,
       ),
-      home: const SchoolApplicationHelper(title: 'School Application Helper'),
+      home: const SchoolApplicationHelper(title: '高校出願情報検索'),
     );
   }
 }
